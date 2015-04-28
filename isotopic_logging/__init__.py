fb4a9d9621a1fe34440a08badb2dda436db11fd5
Alexander Oblovatniy <oblovatniy@gmail.com>
2015-04-29 00:54:36 +0300

    create first tests

--- isotopic_logging/__init__.py
+++ isotopic_logging/__init__.py
@@ -1,1 +1,28 @@
 # -*- coding: utf-8 -*-
+
+from contextlib import contextmanager
+
+
+class PrefixInjector(object):
+
+    def __init__(self, details):
+        self._prefix = self._get_prefix(details)
+
+    @staticmethod
+    def _get_prefix(details):
+        prefix = ["smth", ]
+
+        if details:
+            prefix.append(details)
+
+        prefix.append("")
+
+        return " | ".join(prefix)
+
+    def mark(self, message):
+        return self._prefix + message
+
+
+@contextmanager
+def prefix_injector(details=None):
+    yield PrefixInjector(details)

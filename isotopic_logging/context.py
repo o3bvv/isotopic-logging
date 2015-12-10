4b7cba0aaa2d409cdb8f735c8a90a350bf5fc6b0
Alexander Oblovatniy <oblovatniy@gmail.com>
2015-12-10 10:52:04 +0200

    #1 | remove privacy from `stack` attribute of `_stack_holder`

--- isotopic_logging/context.py
+++ isotopic_logging/context.py
@@ -12,22 +12,23 @@ ScopeItem = namedtuple('ScopeItem', ['injector', 'parent'])
 class InjectionContext(object):
 
     def __init__(self, injector, nested=False):
-        if not hasattr(_stack_holder, '_stack'):
-            _stack_holder._stack = deque()
+        if not hasattr(_stack_holder, 'stack'):
+            _stack_holder.stack = deque()
             self._create_new_scope(injector)
         elif nested:
+            # TODO: use new injector based on direct injector
             self._create_new_scope(injector)
 
     def _create_new_scope(self, injector):
         item = ScopeItem(injector, self)
-        _stack_holder._stack.append(item)
+        _stack_holder.stack.append(item)
 
     def __enter__(self):
-        return _stack_holder._stack[-1].injector
+        return _stack_holder.stack[-1].injector
 
     def __exit__(self, type, value, traceback):
-        if _stack_holder._stack[-1].parent is self:
-            _stack_holder._stack.pop()
+        if _stack_holder.stack[-1].parent is self:
+            _stack_holder.stack.pop()
 
-        if not _stack_holder._stack:
-            del _stack_holder._stack
+        if not _stack_holder.stack:
+            del _stack_holder.stack

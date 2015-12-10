b13a72b94ac7085386518fb5bfb9f78d81682373
Alexander Oblovatniy <oblovatniy@gmail.com>
2015-12-10 12:50:55 +0200

    add tests for SimplePrefixInjector

--- tests/test_injectors.py
+++ tests/test_injectors.py
@@ -2,25 +2,46 @@
 
 import unittest
 
-from isotopic_logging.injectors import DirectPrefixInjector
+from isotopic_logging.injectors import (
+    DirectPrefixInjector, SimplePrefixInjector,
+)
 
 
 class InjectorTestCaseBase(unittest.TestCase):
     strings = [
         "alpha",
         "bravo",
-        "charlie",
     ]
 
 
 class DirectPrefixInjectorTestCase(InjectorTestCaseBase):
 
-    def test_direct_prefix_injector(self):
+    def test_injector(self):
         injector = DirectPrefixInjector("foo > ")
         expected = [
             "foo > alpha",
             "foo > bravo",
-            "foo > charlie",
+        ]
+        result = list(map(injector.mark, self.strings))
+        self.assertEqual(result, expected)
+
+
+class SimplePrefixInjectorTestCase(InjectorTestCaseBase):
+
+    def test_default_delimiter(self):
+        injector = SimplePrefixInjector("foo")
+        expected = [
+            "foo | alpha",
+            "foo | bravo",
+        ]
+        result = list(map(injector.mark, self.strings))
+        self.assertEqual(result, expected)
+
+    def test_custom_delimiter(self):
+        injector = SimplePrefixInjector("[foo]", delimiter=' ')
+        expected = [
+            "[foo] alpha",
+            "[foo] bravo",
         ]
         result = list(map(injector.mark, self.strings))
         self.assertEqual(result, expected)

e14e5b9711ed0d0b06f5e2c3f9d15db4320abddd
Alexander Oblovatniy <oblovatniy@gmail.com>
2015-12-10 13:43:50 +0200

    add tests for AutoprefixInjectorTestCase

--- tests/test_injectors.py
+++ tests/test_injectors.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 
+import mock
 import unittest
 
+from functools import wraps
+from itertools import cycle
+
 from isotopic_logging.injectors import (
-    DirectPrefixInjector, SimplePrefixInjector,
+    DirectPrefixInjector, SimplePrefixInjector, AutoprefixInjector,
 )
 
 
@@ -31,7 +35,7 @@ class DirectPrefixInjectorTestCase(InjectorTestCaseBase):
 
 class SimplePrefixInjectorTestCase(InjectorTestCaseBase):
 
-    def test_default_delimiter(self):
+    def test_delimiter_is_default(self):
         injector = SimplePrefixInjector("foo")
         expected = [
             "foo | alpha",
@@ -39,10 +43,93 @@ class SimplePrefixInjectorTestCase(InjectorTestCaseBase):
         ]
         self.assert_injector(injector, expected)
 
-    def test_custom_delimiter(self):
-        injector = SimplePrefixInjector("[foo]", delimiter=' ')
+    def test_delimiter_is_custom(self):
+        injector = SimplePrefixInjector("foo", delimiter="-")
         expected = [
-            "[foo] alpha",
-            "[foo] bravo",
+            "foo-alpha",
+            "foo-bravo",
+        ]
+        self.assert_injector(injector, expected)
+
+
+def patch_default_generator(call):
+
+    @wraps(call)
+    def decorator(*args, **kwargs):
+        mock_generator = cycle(["foo", "bar", ])
+
+        def generate_prefix(generator=None):
+            return next(generator or mock_generator)
+
+        patcher = mock.patch("isotopic_logging.injectors.generate_prefix")
+        mock_generate_prefix = patcher.start()
+        mock_generate_prefix.side_effect = generate_prefix
+
+        try:
+            call(*args, **kwargs)
+        finally:
+            patcher.stop()
+
+    return decorator
+
+
+class AutoprefixInjectorTestCase(InjectorTestCaseBase):
+
+    @patch_default_generator
+    def test_all_parameters_are_default(self):
+        injector = AutoprefixInjector()
+        expected = [
+            "foo | alpha",
+            "foo | bravo",
+        ]
+        self.assert_injector(injector, expected)
+
+        injector = AutoprefixInjector()
+        expected = [
+            "bar | alpha",
+            "bar | bravo",
+        ]
+        self.assert_injector(injector, expected)
+
+    def test_generator_is_custom(self):
+        generator = cycle(["buz", "qux", ])
+
+        injector = AutoprefixInjector(generator)
+        expected = [
+            "buz | alpha",
+            "buz | bravo",
+        ]
+        self.assert_injector(injector, expected)
+
+        injector = AutoprefixInjector(generator)
+        expected = [
+            "qux | alpha",
+            "qux | bravo",
+        ]
+        self.assert_injector(injector, expected)
+
+    @patch_default_generator
+    def test_delimiter_is_custom(self):
+        injector = AutoprefixInjector(delimiter='-')
+        expected = [
+            "foo-alpha",
+            "foo-bravo",
+        ]
+        self.assert_injector(injector, expected)
+
+    def test_all_parameters_are_custom(self):
+        generator = cycle(["buz", "qux", ])
+
+        injector = AutoprefixInjector(generator, delimiter='-')
+        expected = [
+            "buz-alpha",
+            "buz-bravo",
+        ]
+        self.assert_injector(injector, expected)
+
+        injector = AutoprefixInjector(generator, delimiter='-')
+        expected = [
+            "qux-alpha",
+            "qux-bravo",
         ]
         self.assert_injector(injector, expected)

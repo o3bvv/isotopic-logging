3ad8c5b4a9b3307126e938ccd2cea17b45b5e572
Alexander Oblovatniy <oblovatniy@gmail.com>
2015-12-11 15:48:04 +0200

    move context factories into `context` module

--- isotopic_logging/__init__.py
+++ isotopic_logging/__init__.py
@@ -1,42 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from contextlib import contextmanager
+from .context import direct_injector, prefix_injector # NOQA
+from .context import autoprefix_injector, hybrid_injector # NOQA
 
-from .context import InjectionContext
 from .generators import generate_oid, default_oid_generator # NOQA
-from .injectors import (
-    DirectPrefixInjector, SimplePrefixInjector, AutoprefixInjector,
-    HybrydPrefixInjector,
-)
 from .prefix import make_prefix, join_prefix # NOQA
-from .proxy import InjectingLogger
-
-
-def direct_injector(prefix, inherit=False):
-    return InjectionContext(
-        lambda: DirectPrefixInjector(prefix),
-        inherit)
-
-
-def prefix_injector(prefix, delimiter=None, inherit=False):
-    return InjectionContext(
-        lambda: SimplePrefixInjector(prefix, delimiter),
-        inherit)
-
-
-def autoprefix_injector(oid_generator=None, delimiter=None, inherit=False):
-    return InjectionContext(
-        lambda: AutoprefixInjector(oid_generator, delimiter),
-        inherit)
-
-
-def hybrid_injector(prefix, oid_generator=None, delimiter=None, inherit=False):
-    return InjectionContext(
-        lambda: HybrydPrefixInjector(prefix, oid_generator, delimiter),
-        inherit)
-
-
-@contextmanager
-def wrap_logger(logger, injection_context):
-    with injection_context as inj:
-        yield InjectingLogger(logger, inj)
--- isotopic_logging/context.py
+++ isotopic_logging/context.py
@@ -3,6 +3,10 @@
 from collections import deque, namedtuple
 from threading import local
 
+from .injectors import (
+    DirectPrefixInjector, SimplePrefixInjector, AutoprefixInjector,
+    HybrydPrefixInjector,
+)
 from .injectors import merge_injectors
 
 
@@ -62,3 +66,27 @@ class InjectionContext(object):
     def __exit__(self, type, value, traceback):
         if _stack.top.parent is self:
             _stack.pop()
+
+
+def direct_injector(prefix, inherit=False):
+    return InjectionContext(
+        lambda: DirectPrefixInjector(prefix),
+        inherit)
+
+
+def prefix_injector(prefix, delimiter=None, inherit=False):
+    return InjectionContext(
+        lambda: SimplePrefixInjector(prefix, delimiter),
+        inherit)
+
+
+def autoprefix_injector(oid_generator=None, delimiter=None, inherit=False):
+    return InjectionContext(
+        lambda: AutoprefixInjector(oid_generator, delimiter),
+        inherit)
+
+
+def hybrid_injector(prefix, oid_generator=None, delimiter=None, inherit=False):
+    return InjectionContext(
+        lambda: HybrydPrefixInjector(prefix, oid_generator, delimiter),
+        inherit)
